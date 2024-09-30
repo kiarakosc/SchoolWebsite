6dad7c0d7f3ed1720e0bc4452d9dffb140d9f3e5
kiarakosc <92970829+kiarakosc@users.noreply.github.com>
2024-09-30 22:03:32 +0200

    second try

--- src/main/java/SchoolWebsite.java
+++ src/main/java/SchoolWebsite.java
@@ -21,10 +21,10 @@ public class SchoolWebsite {
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
     }
 
-     @AfterAll
-     static void closeBrowser() {
-         playwright.close();
-     }
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
 
     @BeforeEach
     void createContextAndPage() {
@@ -44,25 +44,21 @@ public class SchoolWebsite {
     void logInAis() {
         page.locator("a[href='https://ais2.upjs.sk/ais/start.do']").click();
         page.fill("#login", "5393452");
-        page.fill("#heslo", "7854");
-        //assertEquals("Clicked", page.evaluate("result"));
+        page.fill("#heslo", "UpJs2524#*");
+        page.locator("#login-form-submit-btn").click();
+        assertEquals("https://ais2.upjs.sk/ais/apps/student/sk/", page.url());
+
     }
+
 
     @Test
     void orderFromEshop() {
-        //  page.locator("a[href='https://eshop.upjs.sk/']").click();
         page.locator("div.header-custom-links a:has-text('E-shop')").click();
+        page.fill("input[name='search']", "pero");
+        page.locator("button.btn.btn-default.btn-lg").click();
 
 
-        Locator parentDiv = page.locator("div.row");
-        Locator childDivs = parentDiv.locator("div.product-layout");
-        System.out.println(childDivs);
-
-
-        List<ElementHandle> divList = childDivs.elementHandles();
-        System.out.println(divList.size());
-        // divList.get(0).click();
-        // page.waitForTimeout(10000);
+         page.waitForTimeout(10000);
 
     }
 
@@ -72,32 +68,39 @@ public class SchoolWebsite {
         page.locator("a[href='https://www.upjs.sk/prirodovedecka-fakulta']").click();
         page.locator("#dropdownMenuButton678").click();
         page.locator("a[href='https://www.upjs.sk/prirodovedecka-fakulta/studium/harm/']:text('Harmonogram akademického roka')").click();
+
+
         page.locator("//h6[contains(text(), 'Harmonogram akademického roka 2024/2025')]").click();
-        page.locator("#download").click();
-
-
-        Download download = page.waitForDownload(() -> {
-
+        Page newPage = context.waitForPage(() -> {
+            page.locator("//h6[contains(text(), 'Harmonogram akademického roka 2024/2025')]").click();
         });
 
-
-        Path downloadedFilePath = download.path();
-
-        if (downloadedFilePath != null && downloadedFilePath.toFile().exists()) {
-            System.out.println("Download successful: " + downloadedFilePath.toString());
-        } else {
-            System.out.println("Download failed.");
-        }
+        assertEquals("https://intranet.upjs.sk/op/op.Public.php?documentid=9351", newPage.url());
 
 
     }
 
     @Test
     void findingMail() {
-        page.locator("a[href='https://ais2.upjs.sk/ais/start.do']").click();
-        page.fill("#login", "5393452");
-        page.fill("#heslo", "7854");
-        //assertEquals("Clicked", page.evaluate("result"));
+        page.locator("#search-query-input").click();
+        page.locator("#search-scope-employees").click();
+        page.fill("#search-query-input", "Juraj Sebej");
+        page.locator("#search-query-submit").click();
+
+        Locator container = page.locator("div.overflow-hidden");
+        Locator trElements = container.locator("tr");
+        List<String> trTexts = trElements.allTextContents();
+        System.out.println(trTexts.size());
+//        for (ElementHandle row : rows) {
+//
+//            ElementHandle emailCell = row.querySelector("td:nth-child(2)");
+//
+//
+//            String email = emailCell.textContent();
+//            System.out.println(email);
+//        }
+
+        page.waitForTimeout(10000);
     }
 
 }
